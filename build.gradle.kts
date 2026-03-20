bc4c26f19c66300d0f1be059911aa40ad26dd79e
Nullicorn <git@nullicorn.me>
2026-03-20 12:44:52 -0400

    Update to latest pre-release 2026.03.20-db226053c

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.hypixel.hytale:Server:2026.03.19-c0865d17c")
+    implementation("com.hypixel.hytale:Server:2026.03.20-db226053c")
 }
 
 fun runServer(task: JavaExec, vararg extraArgs: String) {
